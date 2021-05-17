a568e6ae12677e4171e92c4db478ccf68254ba70
Kenneth Sundqvist <kenneth@kesu.se>
2021-05-17 21:23:12 +0200

    Open new tab directly after current one

--- src/background.js
+++ src/background.js
@@ -1,9 +1,11 @@
-browser.runtime.onMessage.addListener((request) => {
+browser.runtime.onMessage.addListener(async (request) => {
   const url = request?.openUrlInNewTab
   if (typeof url === 'string' && url) {
+    const [currentTab] = await browser.tabs.query({active: true})
     browser.storage.sync.get('activateNewTab', (options) => {
       browser.tabs.create({
         url,
+        index: currentTab.index + 1,
         active: options.activateNewTab,
       })
     })
